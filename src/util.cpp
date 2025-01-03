d08adc00a4a2f71d77c7903a0160f64d54ab7f28
cadaeic628 <sebmorales@ug.uchile.cl>
2025-01-03 15:30:04 -0300

    test: tests created for parseRoutesFromXML and binarySearch example deleted

--- src/util.cpp
+++ src/util.cpp
@@ -1,18 +1,6 @@
 #include "util.h"
 
-int binarySearch(int *v, int n, int x){
-    int l = 0, r = n - 1;
-    while(l <= r){
-        int m = (l + r) / 2;
-        if(v[m] == x) return m;
-        if(v[m] < x) l = m + 1;
-        else r = m - 1;
-    }
-    return -1;
-}
-
-std::map<char*, char*> parse_routes_from_xml(char* filename){
+std::map<char*, char*> parseRoutesFromXML(char* filename){
     std::map<char*, char*> map_vehicles_routes;
-    
-    
+    return map_vehicles_routes;
 }
--- test/utilTest.h
+++ test/utilTest.h
@@ -3,3 +3,8 @@
  * Test the binary search algorithm
  */
 void TestBinarySearch();
+
+/*
+ * Test the parsing of vehicle routes from a .rou.xml file
+ */
+void TestParseRoutesFromXML();
