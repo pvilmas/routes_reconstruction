eec026bc2d55a5dafffbaf85447afed5c477a351
cadaeic628 <sebmorales@ug.uchile.cl>
2025-01-13 23:41:09 -0300

    feat: main finished

--- src/routeReconstruction.cpp
+++ src/routeReconstruction.cpp
@@ -11,16 +11,28 @@ int main(int argc, char *argv[]){
     }
 
     // store paths in variables
-    std::string output_path = argv[1];
-    std::string original_path = argv[2];
-    char* partition_paths = argv[3];
+    std::string output_file = argv[1];
+    std::string osm_file = argv[2];
+    std::string partition_files = argv[3];
 
     // split partition paths by space and store them in an array of strings
-    char* partition_path = strtok(partition_paths, " ");
-    std::string str(partition_path);
+    std::vector<std::string> partition_files_vector = split(partition_files, " ");
 
     // assing routes to vehicles from xml
-    std::map<std::string, std::string> map_vehicles_routes = parseRoutesFromXML(original_path);
+    std::map<std::string, std::string> original_routes = parseRoutesFromXML(osm_file);
 
+    std::map<int, std::map<std::string, std::string>> partitioned_routes;
+    // assign routes to partitions from xml
+    for (int i = 0; i < partition_files_vector.size(); i++) {
+        partitioned_routes[i] = parseRoutesFromXML(partition_files_vector[i]);
+    }
+
+    // reconstruct routes
+    ReconstructedVehicleRoute reconstructed_routes = reconstructRoutes(original_routes, partitioned_routes);
+
+    // save reconstructed routes to json
+    saveToJson(reconstructed_routes, output_file);
+
+    std::cout << "Reconstructed routes saved successfully to " << output_file << "." << std::endl;
     return 0;
 }
